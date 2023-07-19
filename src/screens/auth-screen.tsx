be24dc7f783e31526e664db899e4e129713e79d3
Arif Khan <mak0099@gmail.com>
2023-07-20 01:41:30 +0900

    implemented expo local authentication

--- src/screens/auth-screen.tsx
+++ src/screens/auth-screen.tsx
@@ -1,25 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     StyleSheet,
     View,
     Text,
-    TouchableOpacity
+    TouchableOpacity,
+    Platform,
+    Linking
 } from 'react-native';
 import colors from '../constants/colors';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+import * as LocalAuthentication from 'expo-local-authentication';
 
 const AuthScreen = ({ navigation }: NativeStackScreenProps<any, any>) => {
+    // Define states
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    // Authenticate at the beginning
+    useEffect(() => {
+        authenticate();
+    }, []);
+
+    // Go to the Home screen whenever authentication success
+    useEffect(() => {
+        if (isAuthenticated) {
+            navigation.navigate('HomeScreen')
+        }
+    }, [isAuthenticated]);
+
+    // Authenticate from the user
+    async function authenticate() {
+            const result = await LocalAuthentication.authenticateAsync();
+            setIsAuthenticated(result.success);
+    }
+    async function gotToSettings() {
+
+        // read the device is secured or not
+        const isDeviceSecured = await LocalAuthentication.getEnrolledLevelAsync();
+
+        // if device is not secured go to the device settings
+        if(!isDeviceSecured){
+            Platform.OS === 'ios'
+                ? Linking.openURL('App-Prefs:root=TOUCHID_PASSCODE')
+                : Linking.sendIntent("android.settings.SECURITY_SETTINGS");
+        }
+        authenticate()
+    }
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>
-                Set Authentication to Proceed
-            </Text>
-            <TouchableOpacity
-                style={styles.button}
-                onPress={() => navigation.navigate('HomeScreen')}>
-                <Text style={styles.buttonText}>Go to Settings</Text>
-            </TouchableOpacity>
-        </View>
+        <>
+            <View style={styles.container}>
+                <Text style={styles.text}>
+                    Set Authentication to Proceed
+                </Text>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={gotToSettings}>
+                    <Text style={styles.buttonText}>Go to Settings</Text>
+                </TouchableOpacity>
+            </View>
+        </>
     );
 };
 
